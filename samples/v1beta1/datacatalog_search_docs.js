0a702482fece2cc8ea100f4dff098eb9a4d7144b
Marcelo Costa <mmiranda@ciandt.com>
2019-08-07 16:28:08 -0300

    Make code more user friendly

--- samples/v1beta1/datacatalog_search_docs.js
+++ samples/v1beta1/datacatalog_search_docs.js
@@ -1,60 +1,40 @@
-'use strict';
-
 // This application demonstrates how to perform search operations with the
 // Cloud Data Catalog API.
 
 // For more information, see the README.md under /datacatalog and the
 // documentation at https://cloud.google.com/data-catalog/docs.
 
-const datacatalog = require('@google-cloud/datacatalog').v1beta1;
+async function search() {
 
-/**
- * Search Catalog
- *
- * @param organizationId {string} Your Google Cloud organization ID.
- * @param query {string} Your query string.
- * See: https://cloud.google.com/data-catalog/docs/how-to/search-reference
- * Example: system=bigquery type=dataset
- */
-function search(organizationId, query) {
-  const client = new datacatalog.DataCatalogClient();
+  // -------------------------------
+  // Import required modules.
+  // -------------------------------
+  const { DataCatalogClient } = require('@google-cloud/datacatalog').v1beta1;
+  const datacatalog = new DataCatalogClient();
 
-  const includeOrgIds = [organizationId];
+  // -------------------------------
+  // Set your Google Cloud Organization ID.
+  // -------------------------------
+  // TODO(developer): Uncomment the following line before running the sample.
+  // const organizationId = 111111000000;
+
+  // -------------------------------
+  // Set your custom query.
+  // -------------------------------
+  // TODO(developer): Uncomment the following line before running the sample.
+  // const query = 'type=dataset'
+
+  // Create request.
   const scope = {
-    includeOrgIds: includeOrgIds,
+    includeOrgIds: [organizationId],
   };
   const request = {
     scope: scope,
     query: query,
   };
 
-  client
-    .searchCatalog(request)
-    .then(responses => {
-      const resources = responses[0];
-      for (const resource of resources) {
-        console.log(`Result type: ${resource.searchResultType}`);
-        console.log(`Result subtype: ${resource.searchResultSubtype}`);
-        console.log(`Relative resource name: ${resource.relativeResourceName}`);
-        console.log(`Linked resource: ${resource.linkedResource}\n`);
-      }
-    })
-    .catch(err => {
-      console.error(err);
-    });
+  const [result] = await datacatalog.searchCatalog(request);
+  return result;
 }
 
-const argv = require(`yargs`)
-  .option('organization_id', {
-    demandOption: 'Your Google Cloud organization ID',
-    string: true,
-  })
-  .option('query', {
-    demandOption: 'Your custom query',
-    string: true,
-  }).argv;
-
-search(
-  argv.organization_id,
-  argv.query
-);
+search().then(response => { console.log(response) });
